687032c07b93d544a2a542e238ae34b8b244a964
Duziem Ugoji <tickettoduzz@gmail.com>
2022-11-18 04:09:39 +0100

    mission section added

--- src/app/landing-page/ui/landing-ui.module.ts
+++ src/app/landing-page/ui/landing-ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlacardBoardComponent } from './placard-board/placard-board.component';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { HeroBeltComponent } from './hero-belt/hero-belt.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PlacardBoardComponent } from './components/placard-board/placard-board.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { HeroBeltComponent } from './components/hero-belt/hero-belt.component';
+import { MissionSectionComponent } from './components/mission-section/mission-section.component';
+import { LandingPageComponent } from './landing-page.component';
 
 
 
@@ -12,7 +13,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     PlacardBoardComponent,
     HeroSectionComponent,
     HeroBeltComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    MissionSectionComponent
   ],
   imports: [
     CommonModule
